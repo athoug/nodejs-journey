b7e3e0c0f6204bd081b30ec509ebe7e0a233d8cd
Athoug <athoug.s@gmail.com>
2023-08-10 08:46:51 +0300

    callbacks

---
+++ node-course/playground/3-callbacks.js
@@ -0,0 +1,43 @@
+// asynchronous function
+setTimeout(() => {
+	console.log('two seconds are up');
+}, 2000);
+
+const names = ['andrew', 'jen', 'jess'];
+const shortNames = names.filter((name) => name.length <= 4);
+console.log(shortNames);
+
+const geocode = (address, callback) => {
+	setTimeout(() => {
+		const data = {
+			latitude: 0,
+			longitude: 0,
+		};
+
+		callback(data);
+	}, 2000);
+};
+
+geocode('Riyadh', (data) => {
+	console.log(data);
+});
+
+//
+// Goal: Mess around with the callback pattern
+//
+
+// 1. define an add function that accepts the correct arguments
+// 2. use timeout to simulate a 2 second delay
+// 3. after two seconds, call the callback function with the sum
+// 4. test your work
+
+const add = (num1, num2, callback) => {
+	setTimeout(() => {
+		const sum = num1 + num2;
+		callback(sum);
+	}, 2000);
+};
+
+add(1, 4, (sum) => {
+	console.log(sum);
+});

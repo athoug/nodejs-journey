e721f87230d5258acbc27af60efe736fe2929274
Athoug <athoug.s@gmail.com>
2023-08-14 11:24:31 +0300

    template engine

--- node-course/chapter-05/src/app.js
+++ node-course/chapter-05/src/app.js
@@ -1,24 +1,37 @@
 const path = require('path');
 const express = require('express');
+const { log } = require('console');
 
 const app = express();
 const port = 3000;
 const publicDirectory = path.join(__dirname, '../public');
+const viewsDirectory = path.join(__dirname, '../views');
 
+app.set('view engine', 'hbs'); // this line sets teh templeting engine
+app.set('views', viewsDirectory); // i need to set up the views directory otherwise it wont work
 // a single line to serve the directory, we add it after initiating express
 // this is a middleware it allows us to add stuff to our app
 // in this case we are adding static files
 // in a directory we just declared above
 app.use(express.static(publicDirectory)); // this method is a way to customize our server
-
-console.log(__dirname); // the path to my directory the file lives in
-console.log(__filename); // the path to the file name
-console.log(path.join(__dirname, '../public'));
+console.log(path.join(__dirname, '../views'));
 
 // app.com
 // this is called the root url
-app.get('/', (req, res) => {
-	res.send('<h1>Weather ⛅️</h1>');
+app.get('', (req, res) => {
+	res.render('index', { title: 'Weather', name: 'Andrew meed' });
+});
+
+// about
+app.get('/about', (req, res) => {
+	res.render('about', { title: 'what about me?' });
+});
+
+//help
+app.get('/help', (req, res) => {
+	res.render('help', {
+		message: 'what do you need help with first',
+	});
 });
 
 // app.com/weather

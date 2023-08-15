27fe16af954dbe368a2e376ee577c8656a9f6eee
Athoug <athoug.s@gmail.com>
2023-08-15 09:07:09 +0300

    partials

--- node-course/chapter-05/src/app.js
+++ node-course/chapter-05/src/app.js
@@ -1,17 +1,19 @@
 const path = require('path');
 const express = require('express');
-const { log } = require('console');
+const hbs = require('hbs');
 
 const app = express();
 const port = 3000;
 
 // define paths for express configs
 const publicDirectory = path.join(__dirname, '../public');
-const viewsDirectory = path.join(__dirname, '../templates');
+const viewsDirectory = path.join(__dirname, '../templates/views');
+const partialsDirectory = path.join(__dirname, '../templates/partials');
 
 // setup handle bars engine and views location
 app.set('view engine', 'hbs'); // this line sets teh templeting engine
 app.set('views', viewsDirectory); // i need to set up the views directory otherwise it wont work
+hbs.registerPartials(partialsDirectory);
 
 // a single line to serve the directory, we add it after initiating express
 // this is a middleware it allows us to add stuff to our app
@@ -24,18 +26,20 @@ console.log(path.join(__dirname, '../views'));
 // app.com
 // this is called the root url
 app.get('', (req, res) => {
-	res.render('index', { title: 'Weather', name: 'Andrew meed' });
+	res.render('index', { title: 'Weather', name: 'Athoug' });
 });
 
 // about
 app.get('/about', (req, res) => {
-	res.render('about', { title: 'what about me?' });
+	res.render('about', { title: 'what about me?', name: 'Athoug' });
 });
 
 //help
 app.get('/help', (req, res) => {
 	res.render('help', {
+		title: 'I am here to help 👩🏻‍⚕️',
 		message: 'what do you need help with first',
+		name: 'Athoug',
 	});
 });
 

8c8e42018a7ee4424efecd4adefd0b6af58003a4
Athoug <athoug.s@gmail.com>
2023-08-15 08:43:43 +0300

    customizing directory

--- node-course/chapter-05/src/app.js
+++ node-course/chapter-05/src/app.js
@@ -4,15 +4,20 @@ const { log } = require('console');
 
 const app = express();
 const port = 3000;
-const publicDirectory = path.join(__dirname, '../public');
-const viewsDirectory = path.join(__dirname, '../views');
 
+// define paths for express configs
+const publicDirectory = path.join(__dirname, '../public');
+const viewsDirectory = path.join(__dirname, '../templates');
+
+// setup handle bars engine and views location
 app.set('view engine', 'hbs'); // this line sets teh templeting engine
 app.set('views', viewsDirectory); // i need to set up the views directory otherwise it wont work
+
 // a single line to serve the directory, we add it after initiating express
 // this is a middleware it allows us to add stuff to our app
 // in this case we are adding static files
 // in a directory we just declared above
+// setup static directory to serve
 app.use(express.static(publicDirectory)); // this method is a way to customize our server
 console.log(path.join(__dirname, '../views'));
 
